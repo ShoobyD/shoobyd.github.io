7ea48dc80290e637cc3edcffb552ade538992163
Baruch Mustakis <baruch@pic-time.com>
2020-10-20 11:46:07 +0300

    Create SivanCensored.js

---
+++ userscripts/SivanCensored.js
@@ -0,0 +1,25 @@
+// ==UserScript==
+// @name         WhatsApp Web: Sivan Censored
+// @version      0.5
+// @description  try to take over the world!
+// @author       ShoobyD
+// @include      *web.whatsapp.com*
+// @require      https://shoobyd.github.io/assets/lib/mutation-summary.js
+// ==/UserScript==
+
+//# sourceURL=UserScripts/SivanCensored.js
+
+( function() {
+	'use strict';
+
+	new MutationSummary( {
+		'rootNode' : document.body,
+		'callback' : function( sum ) {
+			for ( var elm of sum[ 0 ].added )
+				elm.innerHTML = elm.innerHTML.replace( /סיוון/g, 'ס☠☠☠ן' );
+		},
+		'queries'  : [ { element : '.selectable-text, .quoted-mention' } ],
+	} );
+
+} )();
+
