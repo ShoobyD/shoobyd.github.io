befa13cc9d9570b6a3416b5979b26171eccfcf1f
ShoobyD <baruch@pic-time.com>
2022-09-08 02:33:12 +0300

    userscripts\BypassAdblockRedirect

---
+++ userscripts/BypassAdblockRedirect.js
@@ -0,0 +1,21 @@
+﻿// ==UserScript==
+// @name         Bypass Adblock Redirect
+// @version      0.1
+// @description  bypass redirecting when adblock is detected
+// @author       Baruch Mustakis (a.k.a. ShoobyD)
+// @include      *themarker.com*
+// @run-at       document-start
+// ==/UserScript==
+
+//# sourceURL=UserScripts/BypassAdblockRedirect.js
+
+( function() {
+	'use strict';
+
+	const log = console.log.bind( console );
+
+	if ( /themarker\.com/.test( location.host ) )
+		unsafeWindow.canRunAds = true;
+
+} )();
+
